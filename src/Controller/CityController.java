44cf7a55253421d65c1aa0e0aa153c32f6bc55ad
HADHRI Anas <hadhri.anas@gmail.com>
2018-10-13 17:48:01 +0200

    modifing city controller

--- src/Controller/CityController.java
+++ src/Controller/CityController.java
@@ -4,7 +4,7 @@ package Controller;
 public class CityController {
 
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD !!");
 
 
     }
