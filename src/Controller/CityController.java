3344767526e6c877b344f2f0d9cec9e266ba6bb2
johanndesoyres <johann.de_soyres@devinci.fr>
2018-10-17 16:53:47 +0200

    create methods setBus, setTaxi, setAccident, setSpeedlimit and update method setTrafficligth

--- src/Controller/CityController.java
+++ src/Controller/CityController.java
@@ -10,30 +10,31 @@ import java.util.Random;
 public class CityController {
 
 
-    public static void setTrafficligth(City city, double randomRedProportion, double randomGreenProportion)
+    public static void setTrafficligth(City city, double RedProportion, double GreenProportion)
     {
 
         //Random Object
         Random random = new Random();
 
 
-        int maxTrafficligth = (int) ( city.getHeight()*city.getWidth()*randomRedProportion);
+        int maxTrafficligth = (int) ( city.getHeight()*city.getWidth()*RedProportion);
         int numTrafficligth=0;
+
 
         while(numTrafficligth <maxTrafficligth) {
 
             int randomRow = random.nextInt(city.getHeight()); // To generate numbrer between 0 and height - 1
             int randomCol = random.nextInt(city.getWidth()); //  To generate numbrer between 0 and width - 1
 
-            //if the node is not occupied and the traffic ligth is not red
-            if (!city.getMatrice()[randomRow][randomCol].isOccupied() && city.getMatrice()[randomRow][randomCol].getMy_traffic_ligth()!= Node.Traffic_ligth.GREEN) {
+            //if the node is not occupied and the traffic ligth is null
+            if (!city.getMatrice()[randomRow][randomCol].isOccupied() && city.getMatrice()[randomRow][randomCol].getMy_traffic_ligth()== null) {
                 city.getMatrice()[randomRow][randomCol].setMy_traffic_ligth(Node.Traffic_ligth.RED);
                 numTrafficligth+=1;
             }
         }
 
 
-        maxTrafficligth = (int) ( city.getHeight()*city.getWidth()*randomGreenProportion);
+        maxTrafficligth = (int) ( city.getHeight()*city.getWidth()*GreenProportion);
         numTrafficligth=0;
 
         while(numTrafficligth <maxTrafficligth) {
@@ -41,8 +42,8 @@ public class CityController {
             int randomRow = random.nextInt(city.getHeight()); // To generate numbrer between 0 and height - 1
             int randomCol = random.nextInt(city.getWidth()); //  To generate numbrer between 0 and width - 1
 
-            //if the node is not occupied and the traffic ligth is not Green
-            if (!city.getMatrice()[randomRow][randomCol].isOccupied() && city.getMatrice()[randomRow][randomCol].getMy_traffic_ligth()!= Node.Traffic_ligth.RED) {
+            //if the node is not occupied and the traffic ligth is null
+            if (!city.getMatrice()[randomRow][randomCol].isOccupied() && city.getMatrice()[randomRow][randomCol].getMy_traffic_ligth()==null) {
                 city.getMatrice()[randomRow][randomCol].setMy_traffic_ligth(Node.Traffic_ligth.GREEN);
                 numTrafficligth+=1;
             }
@@ -50,34 +51,154 @@ public class CityController {
 
     }
 
-    //Just to test the method setTrafficligth
+    public static void setBus(City city, double BusProportion)
+    {
+        //Random Object
+        Random random = new Random();
+
+
+        int maxBus = (int) ( city.getHeight()*city.getWidth()*BusProportion);
+        int numBus=0;
+
+
+        while(numBus<maxBus) {
+
+            int randomRow = random.nextInt(city.getHeight()); // To generate numbrer between 0 and height - 1
+            int randomCol = random.nextInt(city.getWidth()); //  To generate numbrer between 0 and width - 1
+
+            //if the node is not occupied and the node does not already contain a bus
+            if (!city.getMatrice()[randomRow][randomCol].isOccupied() && !city.getMatrice()[randomRow][randomCol].isBus()) {
+                city.getMatrice()[randomRow][randomCol].setBus(true);
+                numBus+=1;
+            }
+        }
+    }
+
+    public static void setTaxi(City city, double TaxiProportion)
+    {
+        //Random Object
+        Random random = new Random();
+
+
+        int maxTaxi = (int) ( city.getHeight()*city.getWidth()*TaxiProportion);
+        int numTaxi=0;
+
+
+        while(numTaxi<maxTaxi) {
+
+            int randomRow = random.nextInt(city.getHeight()); // To generate numbrer between 0 and height - 1
+            int randomCol = random.nextInt(city.getWidth()); //  To generate numbrer between 0 and width - 1
+
+            //if the node is not occupied and the node does not already contain a taxi
+            if (!city.getMatrice()[randomRow][randomCol].isOccupied() && !city.getMatrice()[randomRow][randomCol].isTaxi()) {
+                city.getMatrice()[randomRow][randomCol].setTaxi(true);
+                numTaxi+=1;
+            }
+        }
+    }
+
+    public static void setAccident(City city, double AccidentProportion)
+    {
+        //Random Object
+        Random random = new Random();
+
+
+        int maxAccident = (int) ( city.getHeight()*city.getWidth()*AccidentProportion);
+        int numAccident=0;
+
+
+        while(numAccident<maxAccident) {
+
+            int randomRow = random.nextInt(city.getHeight()); // To generate numbrer between 0 and height - 1
+            int randomCol = random.nextInt(city.getWidth()); //  To generate numbrer between 0 and width - 1
+
+            //if the node is not occupied and the node does not already contain a accident
+            if (!city.getMatrice()[randomRow][randomCol].isOccupied() && !city.getMatrice()[randomRow][randomCol].isAccident()) {
+                city.getMatrice()[randomRow][randomCol].setAccident(true);
+                numAccident+=1;
+            }
+        }
+    }
+
+    public static void setSpeedlimit(City city)
+    {
+        //Random Object
+        Random random = new Random();
+
+        for(int i=0; i<city.getHeight(); i++)
+        {
+            for(int j=0;j<city.getWidth(); j++)
+            {
+                //if the node is not a building
+                if (!city.getMatrice()[i][j].isOccupied()){
+                    // To generate a speed between 30 and 70
+                    int randomSpeedLimit = random.nextInt(5)+3;
+                    randomSpeedLimit*=10;
+
+                    city.getMatrice()[i][j].setSpeed_limit(randomSpeedLimit);
+                }
+
+            }
+        }
+    }
+
+    public static void setCity(City city)
+    {
+
+    }
+
+    //Just to test the set methods
     public static void main(String[] args) {
 
         City city = new City (100,100);
 
         setTrafficligth(city, 0.1,0.2);
+        setBus(city,0.2);
+        setTaxi(city, 0.3);
+        setAccident(city, 0.1);
+        setSpeedlimit(city);
+
+
 
         int comp1=0;
         int comp2=0;
+        int comp3=0;
+        int comp4=0;
+        int comp5=0;
+
 
         for(int i=0;i<city.getHeight();i++)
         {
             for(int j=0;j<city.getWidth();j++)
             {
+                if(city.getMatrice()[i][j].getMy_traffic_ligth()== Node.Traffic_ligth.RED)//or with RED
+                {
+                    comp2+=1;
+                }
                 if(city.getMatrice()[i][j].getMy_traffic_ligth()== Node.Traffic_ligth.GREEN)//or with RED
                 {
                     comp1+=1;
                 }
-
-                if(city.getMatrice()[i][j].getMy_traffic_ligth()== Node.Traffic_ligth.RED)//or with RED
+                if(city.getMatrice()[i][j].isBus())//or with RED
                 {
-                    comp2+=1;
+                    comp3+=1;
+                }
+                if(city.getMatrice()[i][j].isTaxi())//or with RED
+                {
+                    comp4+=1;
+                }
+                if(city.getMatrice()[i][j].isAccident())//or with RED
+                {
+                    comp5+=1;
                 }
             }
 
         }
         System.out.println(comp1);
         System.out.println(comp2);
+        System.out.println(comp3);
+        System.out.println(comp4);
+        System.out.println(comp5);
 
         System.out.println("HELLO WORLD !!");
 
