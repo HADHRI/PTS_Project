f49fa4bd3d7a29c8d3d7bdc927273b96e56ef911
HADHRI Anas <hadhri.anas@gmail.com>
2018-10-22 01:22:14 +0200

    Methode setAdjancyMatrix

--- src/Controller/Graph.java
+++ src/Controller/Graph.java
@@ -26,21 +26,74 @@ public class Graph {
     public  void setListOfAllNodes()
     {
         for(int i=0;i<city.getHeight();i++)
+    {
+        for(int j=0;j<city.getWidth();j++)
         {
-            for(int j=0;j<city.getWidth();j++)
-            {
-                // IF It's not a building then we add it in out GRAPH
-                if (!city.getMatrice()[i][j].isOccupied())
+            // IF It's not a building then we add it in out GRAPH
+            if (!city.getMatrice()[i][j].isOccupied())
 
-                    listOfAllNodes.add(city.getMatrice()[i][j]);
-            }
-
+                listOfAllNodes.add(city.getMatrice()[i][j]);
         }
+
+    }
     }
 
     //In this methode we will store all relations between nodes ( means we will store edges )
     // At the begining we will consider our graph unweighted .
-    public void setAdjacencyMatrixt(){
+    public void setAdjacencyMatrixt()
+    {
+        for(int i=0;i<city.getHeight();i++)
+        {
+            for(int j=0;j<city.getWidth();j++)
+            {
+                // we have to check if neighbors are not buildings
+
+                //checking Up
+                if(i-1>=0)
+                {
+
+                }
+                //cheking UP Right
+                if (i-1>=0 && j+1<city.getWidth())
+                {
+
+
+                }
+
+                //checking right
+                if (j+1 < city.getWidth())
+                {
+
+                }
+                //checking downRight
+                if( i+1 <city.getHeight() && j+1<city.getWidth())
+                {
+
+                }
+                //checking down
+                if (i+1 <city.getHeight())
+                {
+
+                }
+                //checking down Left
+                if (i+1 <city.getHeight() && j-1 >= 0){
+
+                }
+
+                //checking left
+                if (j-1 >=0){
+
+                }
+
+                //checking UP LEFT
+                if(i-1 >=0 && j-1 >=0){
+
+                }
+
+
+            }
+
+        }
 
 
     }
